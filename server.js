327afeda208e55ec9c83e2ab3bb7b95246fd7792
RoBo_ <mannvender001@gmail.com>
2018-01-18 21:49:00 +0530

    Using port from env if possible

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
+const Server_Port = process.env.PORT || 2222
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -9,5 +10,5 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join('public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(2222, () => console.log('Server started at http://localhost:2222')
+app.listen(Server_Port, () => console.log('Server started at http://localhost:2222')
 )
